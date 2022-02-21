dbb5cb71916aecd7a7b4dd2f9b9d4f15f4f0eac6
mojtabasharafiyeh <moj.sharafiyeh@gmail.com>
2022-02-21 19:58:04 +0330

    spliting component smaller

--- src/component/ExpenceItem.js
+++ src/component/ExpenceItem.js
@@ -1,15 +1,13 @@
 import './ExpenseItem.css'
+import './ExpenseDate'
+import ExpenseDate from './ExpenseDate'
 function ExpenceItem(props) {
   const month = props.date.toLocaleString('en-US', { month: 'long' })
   const day = props.date.toLocaleString('en-US', { day: '2-digit' })
   const year = props.date.getFullYear()
   return (
     <div className='expense-item'>
-      <div>
-        <div>{month}</div>
-        <div>{year}</div>
-        <div>{day}</div>
-      </div>
+      <ExpenseDate month={month} day={day} year={year} />
       <div className='expense-item__description'>
         <h2> {props.title}</h2>
         <div className='expense-item__price'>${props.amount}</div>
