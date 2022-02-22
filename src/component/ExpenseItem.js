6f34bbe35a8f339d380aad50599054fa287f40f8
mojtabasharafiyeh <moj.sharafiyeh@gmail.com>
2022-02-22 18:23:50 +0330

    work whith composition of components(implement card component)

--- src/component/ExpenseItem.js
+++ src/component/ExpenseItem.js
@@ -1,18 +1,19 @@
 import './ExpenseItem.css'
 import './ExpenseDate'
 import ExpenseDate from './ExpenseDate'
+import Card from './Card'
 function ExpenseItem(props) {
   const month = props.date.toLocaleString('en-US', { month: 'long' })
   const day = props.date.toLocaleString('en-US', { day: '2-digit' })
   const year = props.date.getFullYear()
   return (
-    <div className='expense-item'>
+    <Card className='expense-item'>
       <ExpenseDate month={month} day={day} year={year} />
       <div className='expense-item__description'>
         <h2> {props.title}</h2>
         <div className='expense-item__price'>${props.amount}</div>
       </div>
-    </div>
+    </Card>
   )
 }
 export default ExpenseItem
