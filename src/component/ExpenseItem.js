c88e33eaed999fd183d8c05b514212a8f89d1b20
mojtabasharafiyeh <moj.sharafiyeh@gmail.com>
2022-02-22 21:03:12 +0330

    add click handler function

--- src/component/ExpenseItem.js
+++ src/component/ExpenseItem.js
@@ -6,12 +6,16 @@ function ExpenseItem(props) {
   const month = props.date.toLocaleString('en-US', { month: 'long' })
   const day = props.date.toLocaleString('en-US', { day: '2-digit' })
   const year = props.date.getFullYear()
+  const clickHandler = () => {
+    console.log('clicked')
+  }
   return (
     <Card className='expense-item'>
       <ExpenseDate month={month} day={day} year={year} />
       <div className='expense-item__description'>
         <h2> {props.title}</h2>
         <div className='expense-item__price'>${props.amount}</div>
+        <button onClick={clickHandler}>click me</button>
       </div>
     </Card>
   )
