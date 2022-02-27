b8bf35f7a5b308c4ab89cef0c8cfb141b940b43b
mojtabasharafiyeh <moj.sharafiyeh@gmail.com>
2022-02-27 22:08:51 +0330

    add form

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import Expenses from './component/Expenses'
-
+import NewExpenses from './component/NewExpenses/NewExpenses'
 function App() {
   const expenses = [
     {
@@ -25,6 +25,7 @@ function App() {
   return (
     <div>
       <h2>Let's get started!</h2>
+      <NewExpenses />
       <Expenses items={expenses} />
     </div>
   )
---
+++ src/component/NewExpenses/NewExpenses.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import ExpenseForm from './ExpenseForm'
+import './NewExpenses.css'
+
+const NewExpenses = () => {
+  return (
+    <div className='new-expense'>
+      <ExpenseForm></ExpenseForm>
+    </div>
+  )
+}
+export default NewExpenses
