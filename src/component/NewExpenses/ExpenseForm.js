4f891f4d4f8b7e9e1bd472ad6d1490d3ae0e8483
mojtabasharafiyeh <moj.sharafiyeh@gmail.com>
2022-02-28 20:04:46 +0330

    updating state depends on prev state

--- src/component/NewExpenses/ExpenseForm.js
+++ src/component/NewExpenses/ExpenseForm.js
@@ -2,18 +2,29 @@ import React, { useState } from 'react'
 import './ExpenseForm.css'
 
 const ExpenseForm = () => {
-  const [enteredTitle, setenteredTitle] = useState('')
-  const [enteredAmount, setenteredAmount] = useState('')
-  const [enteredDate, setenteredDate] = useState('')
+  // const [enteredTitle, setenteredTitle] = useState('')
+  // const [enteredAmount, setenteredAmount] = useState('')
+  // const [enteredDate, setenteredDate] = useState('')
+  const [userInput, setuserInput] = useState({
+    enteredTitle: '',
+    enteredAmount: '',
+    enteredDate: '',
+  })
 
   const titleChangeHandler = (event) => {
-    setenteredTitle(event.target.value)
+    // setenteredTitle(event.target.value)
+    setuserInput((prevstate) => {
+      return {
+        ...prevstate,
+        enteredTitle: event.target.value,
+      }
+    })
   }
   const amountChangeHandler = (event) => {
-    setenteredAmount(event.target.value)
+    // setenteredAmount(event.target.value)
   }
   const dateChangeHandler = (event) => {
-    setenteredDate(event.target.value)
+    // setenteredDate(event.target.value)
   }
 
   return (
