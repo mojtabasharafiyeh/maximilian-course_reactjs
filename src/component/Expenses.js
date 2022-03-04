e81510f57f64c826fd5ab14193c46d3c9b48ff3c
mojtaba <61268429+mojtabasharafiyeh@users.noreply.github.com>
2022-03-04 18:52:11 +0330

    add controlled component

--- src/component/Expenses.js
+++ src/component/Expenses.js
@@ -2,15 +2,21 @@ import ExpenseItem from './ExpenseItem'
 import './Expenses.css'
 import Card from '../UI/Card'
 import ExpensesFilter from './ExpenseFilter'
-import { useState } from 'react'
+import React, { useState } from 'react'
+
 const Expenses = (props) => {
-  const [filter, setfilter] = useState('2020')
-  const selectHandler = (e) => {
-    setfilter(e)
+  const [filteredYear, setFilteredYear] = useState('2020')
+
+  const filterChangeHandler = (selectedYear) => {
+    setFilteredYear(selectedYear)
   }
   return (
     <Card className='expenses'>
-      <ExpensesFilter selected={filter} onselect={selectHandler} />
+      <ExpensesFilter
+        selected={filteredYear}
+        onChangeFilter={filterChangeHandler}
+      />
+
       <ExpenseItem
         title={props.items[0].title}
         amount={props.items[0].amount}
