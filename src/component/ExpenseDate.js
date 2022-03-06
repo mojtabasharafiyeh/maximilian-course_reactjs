52f3d4a742e0f6ab5108b87557ae6a77f110e874
mojtaba <61268429+mojtabasharafiyeh@users.noreply.github.com>
2022-03-06 23:15:14 +0330

    conditional rendering

--- src/component/ExpenseDate.js
+++ src/component/ExpenseDate.js
@@ -1,10 +1,13 @@
 import './ExpenseDate.css'
 const ExpenseDate = (props) => {
+  const month = props.date.toLocaleString('en-US', { month: 'long' })
+  const day = props.date.toLocaleString('en-US', { day: '2-digit' })
+  const year = props.date.getFullYear()
   return (
     <div className='expense-date'>
-      <div className='expense-date__month'>{props.month}</div>
-      <div className='expense-year'>{props.year}</div>
-      <div className='expense-date__day'>{props.day}</div>
+      <div className='expense-date__month'>{month}</div>
+      <div className='expense-year'>{year}</div>
+      <div className='expense-date__day'>{day}</div>
     </div>
   )
 }
--- src/component/ExpenseItem.js
+++ src/component/ExpenseItem.js
@@ -4,12 +4,9 @@ import './ExpenseDate'
 import ExpenseDate from './ExpenseDate'
 import Card from '../UI/Card'
 function ExpenseItem(props) {
-  const month = props.date.toLocaleString('en-US', { month: 'long' })
-  const day = props.date.toLocaleString('en-US', { day: '2-digit' })
-  const year = props.date.getFullYear()
   return (
     <Card className='expense-item'>
-      <ExpenseDate month={month} day={day} year={year} />
+      <ExpenseDate date={props.date} />
       <div className='expense-item__description'>
         <h2> {props.title}</h2>
         <div className='expense-item__price'>${props.amount}</div>
